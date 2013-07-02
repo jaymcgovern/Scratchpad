d7626f60a6dca941debebe615ce03e798604bf72
jaymcgovern <jmcgovern@usatoday.com>
2013-07-02 13:45:37 -0400

    Added Scratchpad view to Django site

--- Scratchpad/Scratchpad/views.py
+++ Scratchpad/Scratchpad/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def hello(request):
 	return HttpResponse("Hello, Jay!!!")
+
+def scratchpad(request):
+	return HttpResponse("<h1>Welcome to Scratchpad!</h1>")
