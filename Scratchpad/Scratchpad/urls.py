0bac77cf5c4897908f99a3771a43de0aaff01f3f
jaymcgovern <jmcgovern@usatoday.com>
2013-07-02 13:39:04 -0400

    Added new links view to Django site

--- Scratchpad/Scratchpad/urls.py
+++ Scratchpad/Scratchpad/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from Scratchpad.views import hello
+from Scratchpad.views import hello, links
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -16,5 +16,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^hello/$', hello)
+    url(r'^hello/$', hello),
+    url(r'^links/$', links)
 )
--- Scratchpad/Scratchpad/views.py
+++ Scratchpad/Scratchpad/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def hello(request):
 	return HttpResponse("Hello, Jay!!!")
+
+def links(request):
+	return HttpResponse("<a>http://google.com</a>")
